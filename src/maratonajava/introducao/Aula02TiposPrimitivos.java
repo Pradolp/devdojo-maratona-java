d2231401578733ab5a8702c4102c9ba7b63d6b09
Lucas Prado <pradolucassilvap@gmail.com>
2026-07-12 17:41:32 -0300

    Aula de operadores lógicos e aritméticos

---
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double wageDouble = 2000;
+        float wageFloat = 2500;
+        byte ageBye = 13;
+        short ageShort = 24;
+        long ageLong = 21;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
+
+        System.out.println(age);
+        System.out.println(age);
+    }
+}
---
+++ src/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,20 @@
+package maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args){
+        // + - / *
+
+        double a = 10;
+        double b = 20;
+
+        double resultAdd = 10 + 20;
+        double resultSub = 10 - 20;
+        double resultMult = 10 * 20;
+        double resultDiv = 10 / (double) 20;
+
+        // &&(AND) ||(OR)
+        // Testando o debug
+        boolean isTenBiggerThenFiveOrFour = 10 >= 4 || 10 >= 5;
+
+    }
+}
